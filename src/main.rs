3c3a65c613932e2ab21cfd1b138d1f8736bc9276
thepigcat <benpospo@gmail.com>
2023-07-29 18:28:44 +0200

    update code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,44 @@
 use std::env;
 
-use serenity::async_trait;
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::prelude::*;
+use serenity::{
+    async_trait,
+    model::{channel::Message, gateway::Ready},
+    prelude::*,
+};
+
+const HELP_MESSAGE: &str = "
+Hello there, Human!
+
+You have summoned me. Let's see about getting you what you need.
+
+❓ Need technical help?
+➡️ Post in the <#CHANNEL_ID> channel and other humans will assist you.
+
+❓ Looking for the Code of Conduct?
+➡️ Here it is: <https://opensource.facebook.com/code-of-conduct>
+
+❓ Something wrong?
+➡️ You can flag an admin with @admin
+
+I hope that resolves your issue!
+
+— HelpBot 🤖
+";
+
+const HELP_COMMAND: &str = "!help";
 
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // Set a handler for the `message` event - so that whenever a new message
-    // is received - the closure (or function) passed will be called.
-    //
-    // Event handlers are dispatched through a threadpool, and so multiple
-    // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
 
-    // Set a handler to be called on the `ready` event. This is called when a
-    // shard is booted, and a READY payload is sent by Discord. This payload
-    // contains data like the current user's guild Ids, current user data,
-    // private channels, and more.
-    //
-    // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
     }
@@ -39,23 +46,14 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    // Configure the client with your Discord bot token in the environment.
-    let token = env::var("MTEzMDEwMTQyNjAwMjA2NzUxNw.GwjFzV.YnzV5GcjhhnYOCR6PQ2LBH2xu6WpSvSTKTjrHI").expect("Expected a token in the environment");
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
 
-    // Create a new instance of the Client, logging in as a bot. This will
-    // automatically prepend your bot token with "Bot ", which is a requirement
-    // by Discord for bot users.
-    let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    let mut client = Client::new(&token)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
 
-    // Finally, start a single shard, and start listening to events.
-    //
-    // Shards will automatically attempt to reconnect, and will perform
-    // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
     }
